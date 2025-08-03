4db897a00c171adc095dc0a163709e46b1704d04
Ruslan <Rahimbayr@yandex.ru>
2025-08-04 01:12:26 +0400

    DataTypes

---
+++ src/Lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,105 @@
+package Lesson2;
+
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        byte b1 = 12; // Десятеричное счисление
+        byte b2 = 0b1100; // Двоичное счисление
+        byte b3 = 014; // Восьмеричное счисление
+        byte b4 = 0xC; // Шестнадцатеричное счисление
+
+        System.out.println("Десятеричное счисление тип byte: " + b1);
+        System.out.println("Двоичное счисление тип byte: " + b2);
+        System.out.println("Восьмеричное счисление тип byte: " + b3);
+        System.out.println("Шестнадцатеричное счисление тип byte: " + b4);
+        System.out.println();
+        System.out.println();
+
+
+        short s1 = 1300; // Десятеричное счисление
+        short s2 = 0b10100010100; // Двоичное счисление
+        short s3 = 02424; // Восьмеричное счисление
+        short s4 = 0x514; // Шестнадцатеричное счисление
+
+        System.out.println("Десятеричное счисление тип short: " + s1);
+        System.out.println("Двоичное счисление тип short: " + s2);
+        System.out.println("Восьмеричное счисление тип short: " + s3);
+        System.out.println("Шестнадцатеричное счисление тип short: " + s4);
+        System.out.println();
+        System.out.println();
+
+
+        int i1 = 0; // Десятеричное счисление
+        int i2 = 0b0; // Двоичное счисление
+        int i3 = 00; // Восьмеричное счисление
+        int i4 = 0x0; // Шестнадцатеричное счисление
+
+        System.out.println("Десятеричное счисление тип int: " + i1);
+        System.out.println("Двоичное счисление тип int: " + i2);
+        System.out.println("Восьмеричное счисление тип int: " + i3);
+        System.out.println("Шестнадцатеричное счисление тип int: " + i4);
+        System.out.println();
+        System.out.println();
+
+
+        long l1 = 123456789 ;
+        long l2 = 0b111010110111100110100010101L;
+        long l3 = 0726746425 ;
+        long l4 = 0x75BCD15;
+
+
+        System.out.println("Десятеричное счисление тип long: " + l1);
+        System.out.println("Двоичное счисление тип long: " + l2);
+        System.out.println("Восьмеричное счисление тип long: " + l3);
+        System.out.println("Шестнадцатеричное счисление тип long: " + l4);
+        System.out.println();
+        System.out.println();
+
+
+
+        float f1 = 2.22f;
+        float f2 = 2.2f;
+        double d1 = 22.22d;
+        double d2 = 222.222d;
+        boolean bb1 = true;
+        boolean bb2 = false;
+
+        System.out.println("Дробные числа float: " + f1);
+        System.out.println("Дробные числа float: " + f2);
+        System.out.println("Дробные числа double: " + d1);
+        System.out.println("Дробные числа double: " + d2);
+        System.out.println("Правда: " + bb1);
+        System.out.println("Ложь: " + bb2);
+        System.out.println();
+        System.out.println();
+
+
+
+        char c1 = '2';
+        char c2 = '(';
+        char c3 = 'G';
+        char c4 = 888;
+        char c5 = '\u2613';
+        char c6  = '0';
+
+        System.out.println("Строка char: " + c1);
+        System.out.println("Строка char: " + c2);
+        System.out.println("Строка char: " + c3);
+        System.out.println("Строка char: " + c4);
+        System.out.println("Строка char: " + c5);
+        System.out.println("Строка char: " + c6);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
